ad7d7204cbd18518f4fc3c7d444d610566e33930
Anton <apolkhovskiy@dataart.com>
2015-01-24 18:22:25 +0200

    adding basic tests

---
+++ Hello-SelfHost-WebAPI/BasicTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web.Security;
+using Hello_SelfHost_WebAPI.Start;
+using NUnit.Framework;
+
+namespace Hello_SelfHost_WebAPI
+{
+    class BasicTests
+    {
+        readonly ApiRunner _runner = new ApiRunner();
+
+        [Test]
+        public void InitTest()
+        {
+            _runner.Start();
+            _runner.Stop();
+            Assert.Throws<InvalidOperationException>(() => _runner.Stop());
+            _runner.Start();
+            Assert.Throws<InvalidOperationException>(() => _runner.Start());
+            _runner.Stop();
+        }
+
+        [Test]
+        public void AuthenticationTest()
+        {
+            Membership.DeleteUser("admin");
+            _runner.Start();
+            MembershipCreateStatus createStatus;
+            Membership.CreateUser("admin", "Admin2015!", "admin@admin.com", "Question?", "Answer", true,
+                out createStatus);
+            Assert.AreEqual(createStatus, MembershipCreateStatus.Success);
+
+            var result = GetValues("admin", "incorrect");
+            Assert.AreEqual(result.StatusCode, HttpStatusCode.Unauthorized);
+            result = GetValues("admin", "Admin2015!");
+            Assert.AreEqual(result.StatusCode, HttpStatusCode.OK);
+            var readStream = new StreamReader(result.GetResponseStream(), Encoding.UTF8);
+            Assert.AreEqual(readStream.ReadToEnd(), "[\"value1\",\"value2\"]");
+            _runner.Stop();
+        }
+
+        private static HttpWebResponse GetValues(string login, string password)
+        {
+            HttpWebResponse result = null;
+            try
+            {
+                var request = WebRequest.Create("http://localhost:9000/api/values");
+                request.Credentials = new NetworkCredential(login, password);
+                result = (HttpWebResponse)(request.GetResponse());
+            }
+            catch (Exception exception)
+            {
+                var webException = exception as WebException;
+                if (webException != null)
+                {
+                    var response = webException.Response as HttpWebResponse;
+                    if (response != null)
+                    {
+                        result = response;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
--- Hello-SelfHost-WebAPI/Program.cs
+++ Hello-SelfHost-WebAPI/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Net;
-using System.Text;
-using System.Web.Security;
 using Hello_SelfHost_WebAPI.Start;
 
 namespace Hello_SelfHost_WebAPI
@@ -13,49 +9,9 @@ namespace Hello_SelfHost_WebAPI
         {
             var runner = new ApiRunner();
             runner.Start();
-
-            MembershipCreateStatus createStatus;
-            Membership.CreateUser("admin", "Admin2015!", "admin@admin.com", "Question?", "Answer", true,
-                out createStatus);
-            Console.WriteLine("Added user admin with password Admin2015!");
-            Console.WriteLine("Trying to authenticate with admin/incorrect");
-            var result = GetValues("admin", "incorrect");
-            Console.WriteLine("Got: " + result.StatusDescription);
-            if (result.StatusCode == HttpStatusCode.Unauthorized)
-            {
-                Console.WriteLine("Trying to authenticate with admin/Admin2015!");
-                result = GetValues("admin", "Admin2015!");
-                var readStream = new StreamReader(result.GetResponseStream(), Encoding.UTF8);
-                Console.WriteLine("Response: " + readStream.ReadToEnd());
-            }
-
             Console.WriteLine("Press ENTER to exit the app.");
             Console.ReadLine();
             runner.Stop();
-        }
-
-        private static HttpWebResponse GetValues(string login, string password)
-        {
-            HttpWebResponse result = null;
-            try
-            {
-                var request = WebRequest.Create("http://localhost:9000/api/values");
-                request.Credentials = new NetworkCredential(login, password);
-                result = (HttpWebResponse) (request.GetResponse());
-            }
-            catch (Exception exception)
-            {
-                var webException = exception as WebException;
-                if (webException != null)
-                {
-                    var response = webException.Response as HttpWebResponse;
-                    if (response != null)
-                    {
-                        result = response;
-                    }
-                }
-            }
-            return result;
         }
     }
 }
